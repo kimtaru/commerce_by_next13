4801b763029c6e9daef26912b571190a73ab91ed
kimtaru <kimtaru2@gmail.com>
2022-11-30 14:32:17 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Layout from "../components/layout/layout";
 
 export default function Home() {
-  return <div className="text-center p-12 bg-slate-400">추천상품리스트</div>;
+  return <div className="text-center p-12 bg-slate-400">추천상품리스트!!</div>;
 }
 
 Home.getLayout = function getLayout(page) {
